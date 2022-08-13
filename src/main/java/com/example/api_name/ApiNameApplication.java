6b4159a09dc90edf4b755400d47e2a4fbdefb61e
dayann <dayanndeshayes@gmail.com>
2022-08-13 18:52:26 +0200

    configuration du HttpNames

--- src/main/java/com/example/api_name/ApiNameApplication.java
+++ src/main/java/com/example/api_name/ApiNameApplication.java
@@ -1,13 +1,23 @@
 package com.example.api_name;
 
+import com.example.http.HttpNames;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"project.names.controller", "project.names.service", "project.names.dao"})
+//@EnableJpaRepositories("project.names.dao")
 public class ApiNameApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ApiNameApplication.class, args);
+
+        HttpNames httpOrigin = new HttpNames();
+        String page = httpOrigin.getHtmlNamesPage("berber");
+        httpOrigin.getNames(page);
     }
 
 }
--- src/main/java/com/example/http/HttpNames.java
+++ src/main/java/com/example/http/HttpNames.java
@@ -1,16 +1,44 @@
 package com.example.http;
 
+import com.example.model.Name;
 import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.jsoup.Jsoup;//? a rechercher
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
+ @Service
 public class HttpNames {
 
-    public String getHtmlNamesPage(){
-        return "";
+    private final RestTemplate restTemplate = new RestTemplateBuilder().build();// création de notre RestTemplate qui permet d'envoyer ou
+                                                                                // récupérer des données via requêtes HTTP
+    private final String BEHIND_THE_NAME_URL = "https://www.behindthename.com/";
+
+    public String getHtmlNamesPage (String origin){
+        String url = "https://www.behindthename.com/names/usage/" + origin;
+        return this.restTemplate.getForObject(url, String.class);//return la récupération de la page html en string
     }
 
-    public List<JSONPObject> getJsonName(String htmlNamePage){// TODO : remplacer json par name object
-        return null;
+    public List<Name> getNames (String htmlNamesPage){ // TODO : add origin as parameter
+        Document doc = Jsoup.parseBodyFragment(htmlNamesPage);//Crée un document a partir de la page html
+        List<Element> elements = doc.getElementsByClass("browsename");//
+        List<Name> namesList = new ArrayList<>();
+        for (Element el : elements){
+            Name name = new Name();
+            System.err.println(el);
+            name.setName(el.getElementsByClass("listname").get(0).text());
+            name.setLink(BEHIND_THE_NAME_URL + el.getElementsByClass("listname").get(0).select("a").attr("href"));
+            name.setGender(el.getElementsByClass("listgender").get(0).text());
+            name.setDescription(el.getElementsByClass("listusage").get(0).text());
+            namesList.add(name);
+        }
+
+        return namesList;
     }
 }
